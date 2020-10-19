66e7988506bfef20f672fb7aba8005e70a739057
hazuboi <hznram07@gmail.com>
2020-10-19 18:43:12 +0700

    Updates

---
+++ precache-manifest.e883f40c59487ceeb7ace7fbf6c515c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36ae5afe33ab7629f1d9d8c2f265ee0",
+    "url": "/MyPortofolio-ReactJS/index.html"
+  },
+  {
+    "revision": "561142751fb712b2accf",
+    "url": "/MyPortofolio-ReactJS/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "6d7743430ad6fbd1d054",
+    "url": "/MyPortofolio-ReactJS/static/css/main.30b2aa00.chunk.css"
+  },
+  {
+    "revision": "561142751fb712b2accf",
+    "url": "/MyPortofolio-ReactJS/static/js/2.a45e9b37.chunk.js"
+  },
+  {
+    "revision": "8e5ae93e18f66c1d607de05f5b5d011b",
+    "url": "/MyPortofolio-ReactJS/static/js/2.a45e9b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7743430ad6fbd1d054",
+    "url": "/MyPortofolio-ReactJS/static/js/main.cccdbc49.chunk.js"
+  },
+  {
+    "revision": "67d24da5f927955e541a",
+    "url": "/MyPortofolio-ReactJS/static/js/runtime-main.05f431d9.js"
+  }
+]);
